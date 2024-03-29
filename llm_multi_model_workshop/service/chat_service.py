7efc75ec0e6a9fbbd503f7ce3ec75b9ec672fa97
Jason Robison <jrrobison@protonmail.com>
2024-03-29 14:59:27 -0600

    Get feedback from multiple personas

--- llm_multi_model_workshop/service/chat_service.py
+++ llm_multi_model_workshop/service/chat_service.py
@@ -1,67 +1,68 @@
 from llm_multi_model_workshop.chatter.mistral_chatter import MistralChatter
-from llm_multi_model_workshop.chatter.gemini_chatter import GeminiChatter
 from llm_multi_model_workshop.chatter.openai_chatter import OpenAIChatter
 from . import config_service
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-arthur_chatter = OpenAIChatter(
-    model="gpt-3.5-turbo",
+writer_chatter = OpenAIChatter(
+    model="gpt-4",
     system_prompt=config_service.get_writer_config()["persona"],
-    name="Arthur",
+    name=config_service.get_writer_config()["name"],
     max_output_tokens=512,
 )
-luis_chatter = MistralChatter(
+editor_chatter = MistralChatter(
     model="mistral-large-latest",
     system_prompt=config_service.get_editor_config()["persona"],
-    name="Luis",
+    name=config_service.get_editor_config()["name"],
     max_output_tokens=512,
 )
-heidi_chatter = GeminiChatter(
-    model="gemini-pro",
+agent_chatter = OpenAIChatter(
+    model="gpt-4",
     system_prompt=config_service.get_agent_config()["persona"],
-    name="Heidi",
+    name=config_service.get_agent_config()["name"],
     max_output_tokens=512,
 )
-yolanda_chatter = OpenAIChatter(
+publisher_chatter = OpenAIChatter(
     model="gpt-4",
     system_prompt=config_service.get_publisher_config()["persona"],
-    name="Yolanda",
+    name=config_service.get_publisher_config()["name"],
     max_output_tokens=1024,
 )
 
-# Give the writing sample to the personas
-# luis_chatter.add_to_history(question)
-# luis_response = (
-#     "Here is what Luis thinks about the original writing: \n***\n"
-#     + luis_chatter.chat("")
-#     + "\n***\n"
-# )
-
-# heidi_chatter.add_to_history(luis_response)
-# heidi_chatter.add_to_history(question)
-# heidi_response = (
-#     "Here is what Heidi thinks about the original writing: \n***\n"
-#     + heidi_chatter.chat("")
-#     + "\n***\n"
-# )
-
-# arthur_chatter.add_to_history(luis_response)
-# arthur_chatter.add_to_history(heidi_response)
-# arthur_chatter.add_to_history(question)
-# arthur_response = (
-#     "Here is what Arthur thinks about the original writing: \n***\n"
-#     + arthur_chatter.chat("")
-#     + "\n***\n"
-# )
-
-# response = yolanda_chatter.chat(question)
-
 
 def chat(text):
-    request = (
-        "Here is the original writing for critique, written by User: \n***"
-        + text
-        + "\n***\n"
-    )
-    feedbacks = luis_chatter.chat(request)
-    return feedbacks
+    try:
+        request = (
+            "Here is the original writing for critique, written by User: \n***"
+            + text
+            + "\n***\n"
+        )
+        editor_feedback = editor_chatter.chat(request)
+        editor_template = (
+            "Here is what Luis thinks about the original writing: \n***\n"
+            + editor_feedback
+            + "\n***\n"
+        )
+
+        agent_chatter.add_to_history(editor_template)
+        agent_chatter.add_to_history(request)
+        agent_feedback = agent_chatter.chat("")
+        agent_template = (
+            "Here is what Heidi thinks about the original writing: \n***\n"
+            + agent_feedback
+            + "\n***\n"
+        )
+
+        writer_chatter.add_to_history(editor_template)
+        writer_chatter.add_to_history(agent_template)
+        writer_chatter.add_to_history(request)
+        writer_feedback = writer_chatter.chat("")
+
+        publisher_feedback = publisher_chatter.chat(request)
+
+        return [editor_feedback, agent_feedback, writer_feedback, publisher_feedback]
+    except Exception as e:
+        logger.error(e.message)
+        return "I'm sorry, I'm having trouble processing your request. Please try again later."
