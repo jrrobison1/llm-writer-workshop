67b9681291985ecd6171128a7873c580b781c5ee
Jason Robison <jrrobison@protonmail.com>
2024-04-06 01:15:58 -0600

    Fix Gemini support

--- llm_writer_workshop/chatter/gemini_chatter.py
+++ llm_writer_workshop/chatter/gemini_chatter.py
@@ -1,7 +1,10 @@
+import logging
 import os
 import google.generativeai as genai
 
 from llm_writer_workshop.chatter.chatter import Chatter
+
+logger = logging.getLogger(__name__)
 
 
 class GeminiChatter(Chatter):
@@ -19,59 +22,34 @@ class GeminiChatter(Chatter):
         temperature=DEFAULT_TEMPERATURE,
         name
     ):
-        self.gemini_api_key = os.environ.get("GEMINI_API_KEY")
-        genai.configure(api_key=self.gemini_api_key)
-
         self.max_output_tokens = max_output_tokens
         self.temperature = temperature
         self.model = model
-        self.gemini_model = genai.GenerativeModel(self.model)
-        self.gemini_model.system_prompt = system_prompt
+        self.system_prompt = system_prompt
         self.message_history = []
         self.name = name
 
     def chat(self, message):
-        if message != "":
-            user_message = {"role": "user", "parts": [message]}
-            self.message_history.append(user_message)
-        # print(self.message_history)
+        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+        model = genai.GenerativeModel(self.model)
 
-        for message in self.message_history:
-            print(message["role"])
-
-        response = self.gemini_model.generate_content(
-            self.message_history,
-            stream=True,
+        response = model.generate_content(
+            message,
             generation_config=genai.types.GenerationConfig(
                 candidate_count=1,
-                # stop_sequences=['x'],
                 max_output_tokens=self.max_output_tokens,
                 temperature=self.temperature,
             ),
+            # safety_settings={
+            #     "HARM_CATEGORY_HARASSMENT": "BLOCK_NONE",
+            #     "HARM_CATEGORY_HATE_SPEECH": "BLOCK_ONLY_HIGH",
+            #     "HARM_CATEGORY_SEXUALLY_EXPLICIT": "BLOCK_NONE",
+            #     "HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_NONE",
+            # },
         )
+        logger.error(response)
 
-        ai_message = ""
-        for chunk in response:
-            if (
-                chunk.candidates is not None
-                and chunk.candidates[0].content is not None
-                and chunk.candidates[0].content.parts is not None
-                and len(chunk.candidates[0].content.parts) > 0
-            ):
-                response_text = (
-                    chunk.candidates[0].content.parts[0].text.replace("•", "  *")
-                )
-                ai_message += response_text
-
-        self.message_history.append({"role": "model", "parts": [ai_message]})
-
-        return ai_message
+        return response.text
 
     def add_to_history(self, message):
-        if self.message_history is not None:
-            if len(self.message_history) > 0:
-                last_message = self.message_history[-1]
-                last_message["parts"][0] = last_message["parts"][0] + " " + message
-            else:
-                user_message = {"role": "user", "parts": [message]}
-                self.message_history.append(user_message)
+        pass
