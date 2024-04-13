587676afcb88afaf5e1e2aab4ec2b75994d04371
Jason Robison <jrrobison1@hoodsen.me>
2024-04-13 10:15:26 -0600

    Update unit tests

--- service/tests/controller/test_chat_controller.py
+++ service/tests/controller/test_chat_controller.py
@@ -21,7 +21,7 @@ def test_generate_reviews_success(client, mocker):
 
     test_json = {
         "text": "This is a sample review text",
-        "models": [{"role": "agent", "model": "gpt2"}],
+        "models": [{"role": "Agent", "model": "gpt-3.5-turbo"}],
     }
 
     response = client.post("/api/v1/generate-reviews", json=test_json)
@@ -31,30 +31,32 @@ def test_generate_reviews_success(client, mocker):
 
 def test_generate_reviews_no_json(client):
     response = client.post("/api/v1/generate-reviews", data="This is not a JSON")
-    assert response.status_code == 400
+    assert response.status_code == 422
 
 
 def test_generate_reviews_empty_json(client):
     response = client.post("/api/v1/generate-reviews", json={})
-    assert response.status_code == 400
-    assert "Empty JSON" in response.json["error_message"]
+    assert response.status_code == 422
 
 
 def test_generate_reviews_missing_text_field(client):
     response = client.post(
         "/api/v1/generate-reviews", json={"not_text": "This should fail"}
     )
-    assert response.status_code == 400
-    assert "Empty JSON" in response.json["error_message"]
+    assert response.status_code == 422
 
 
 def test_generate_reviews_server_error(client, mocker):
     mocker.patch(
-        "llm_writer_workshop.service.chat_service.ChatService.chat",
+        "llm_writer_workshop.service.chat_service.ChatService.chat_all",
         side_effect=Exception("Unexpected error"),
     )
     response = client.post(
-        "/api/v1/generate-reviews", json={"text": "This will trigger a server error"}
+        "/api/v1/generate-reviews",
+        json={
+            "text": "This is a sample review text",
+            "models": [{"role": "Agent", "model": "gpt-3.5-turbo"}],
+        },
     )
     assert response.status_code == 500
     assert "An server error has occurred" in response.json["error_message"]
